109637ecfc2685e01d34b73c45b9b9c562414e16
Akhilesh <akhi.singh1989@gmail.com>
2025-07-07 22:40:24 +1000

    Update api_server.py

--- backend/api_server.py
+++ backend/api_server.py
@@ -7,7 +7,7 @@ OPENWEATHER_API_KEY = "e8a5d05ade6969b66fef40836799ff44"
 
 @app.get("/weather")
 def get_weather(city: str = Query(...)):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=metric"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={e8a5d05ade6969b66fef40836799ff44}&units=metric"
     res = requests.get(url)
     return res.json()
 
